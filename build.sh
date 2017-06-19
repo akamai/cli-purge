55aa5cb2b7c81a7de987703a283d84d7737fdc01
Davey Shafik <me@daveyshafik.com>
2017-06-19 12:41:16 -0700

    Add version number and fix build names

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-GOOS=darwin GOARCH=amd64 go build -o akamai-purge-mac-amd64 .
-GOOS=linux GOARCH=amd64 go build -o akamai-purge-linux-amd64 .
-GOOS=linux GOARCH=386 go build -o akamai-purge-linux-386 .
-GOOS=windows GOARCH=386 go build -o akamai-purge-windows-386.exe .
-GOOS=windows GOARCH=amd64 go build -o akamai-purge-windows-amd64.exe .
+GOOS=darwin GOARCH=amd64 go build -o akamai-purge-$1-macamd64 .
+GOOS=linux GOARCH=amd64 go build -o akamai-purge-$1-linuxamd64 .
+GOOS=linux GOARCH=386 go build -o akamai-purge-$1-linux386 .
+GOOS=windows GOARCH=386 go build -o akamai-purge-$1-windows386.exe .
+GOOS=windows GOARCH=amd64 go build -o akamai-purge-$1-windowsamd64.exe .
 
